3c6d6e2512a2c32f96d13993df4ac7b3dec6c98a
SrikarAmara <sriks.codes@gmail.com>
2020-12-26 07:56:09 +0530

    started class

--- classifier.py
+++ classifier.py
@@ -1,1 +1,18 @@
- 
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
+
+def dhash(image, hashsize = 8):
+    # convert the image to grayscale and resize the grayscale image,
+    # adding a single column (width) so we can compute the horizontal
+    # gradient
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (hashSize + 1, hashSize))
+    # compute the (relative) horizontal gradient between adjacent
+    # column pixels
+    diff = resized[:, 1:] > resized[:, :-1]
+    # convert the difference image to a hash and return it
+    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
